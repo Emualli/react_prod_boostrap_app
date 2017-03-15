8603cf2dda369dd92b4f7c3bc797a68a89f5a863
Guillaume Degermann <guillaume.deger@gmail.com>
2017-03-16 00:38:44 +0100

    add css && basic todo behaviour

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash'
 
 class App extends Component {
+  constructor() {
+      super()
+      this.state = {
+          todos: [
+              { id: 1, name: 'Task 1', isComplete: false },
+              { id: 2, name: 'Task 2', isComplete: false },
+              { id: 3, name: 'Task 3', isComplete: false }
+          ]
+      }
+  }
+
+  _updateTodo(id) {
+    const currentTodo = this.state.todos.filter(t => { return t.id === id })[0]
+    if (currentTodo) {
+      currentTodo.isComplete = !currentTodo.isComplete
+      this.setState({ todos: _.merge(this.state.todos, currentTodo) }) }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React todos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className='Todo-App'>
+              <form>
+                  <input type='test' />
+              </form>
+              <div className='Todo-list'>
+                  <ul>
+                      {this.state.todos.map((todo) => {
+                          return (<li key={todo.id}><input onChange={this._updateTodo.bind(this, todo.id)} checked={todo.isComplete} type='checkbox'/> {todo.name}</li>)
+                      })}
+                  </ul>
+              </div>
+          </div>
       </div>
     );
   }
