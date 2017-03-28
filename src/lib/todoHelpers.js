467caecb81d96e8ee04d04c3cb2f4c43a69e0ba7
Guillaume Degermann <guillaume.deger@gmail.com>
2017-03-28 23:50:00 +0200

    add more func && tests

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (list, item) => [...list, item]
 
 export const generateId = () => Math.floor(Math.random()*100000)
+
+export const findById = (id, list) => list.find(e => e.id === id)
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete})
+
+export const updateTodo = (list, updatedItem) => {
+    const updatedIndex = list.findIndex(item => item.id === updatedItem.id)
+        return [
+            ...list.slice(0, updatedIndex),
+            updatedItem,
+            ...list.slice(updatedIndex + 1)
+        ]
+}
